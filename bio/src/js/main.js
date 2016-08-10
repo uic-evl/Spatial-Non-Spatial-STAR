6fbe06076fb500052e675286a87b58b957844c6c
timothy <onorinbejasus@gmail.com>
2016-08-09 23:18:49 -0500

    - Started the refactoring of main.js to setup.js

--- bio/src/js/main.js
+++ bio/src/js/main.js
@@ -1,33 +1,5 @@
 var App = App || {};
 
-/** Initialize the advanced menu accordian **/
-$(function() {
-    var Accordion = function(el, multiple) {
-        this.el = el || {};
-        this.multiple = multiple || false;
-
-        // Variables privadas
-        var links = this.el.find('.link');
-        // Evento
-        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-    };
-
-    Accordion.prototype.dropdown = function(e) {
-        var $el = e.data.el;
-        var $this = $(this);
-        var $next = $this.next();
-
-        $next.slideToggle();
-        $this.parent().toggleClass('open');
-
-        if (!e.data.multiple) {
-            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-        }
-    };
-
-    App.accordion = new Accordion($('#accordion'), false);
-});
-
 (function() {
 
     var biology_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KRStNdz1NR1yvZv00l5g1kUnIrpPP0BPSXH_riYCtWk/pubhtml';
--- engineering/src/js/main.js
+++ engineering/src/js/main.js
@@ -1,33 +1,5 @@
 var App = App || {};
 
-/** Initialize the advanced menu accordian **/
-$(function() {
-    var Accordion = function(el, multiple) {
-        this.el = el || {};
-        this.multiple = multiple || false;
-
-        // Variables privadas
-        var links = this.el.find('.link');
-        // Evento
-        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-    };
-
-    Accordion.prototype.dropdown = function(e) {
-        var $el = e.data.el;
-        var $this = $(this);
-        var $next = $this.next();
-
-        $next.slideToggle();
-        $this.parent().toggleClass('open');
-
-        if (!e.data.multiple) {
-            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-        }
-    };
-
-    App.accordion = new Accordion($('#accordion'), false);
-});
-
 (function() {
 
     var engineering_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1b5_Yy_cGvcL6Uec3rYaEgT4NSi29OgP8tU1mV43DcFE/pubhtml';
