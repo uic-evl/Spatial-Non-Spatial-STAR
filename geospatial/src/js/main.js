1d340daf8ed78af2bc99921add3872d9b2c08a97
Juan Trelles <juantrelles@gmail.com>
2016-08-11 10:27:43 -0500

    Geospatial pages
    Adding the geospatial wiki. Spreadsheet checked to avoid typo errors.
    Enjoy has not been added yet as a Task.

---
+++ geospatial/src/js/main.js
@@ -0,0 +1,361 @@
+var App = App || {};
+
+(function() {
+
+    var geospatial_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/10mhjk1rem8QPK8QGE7BzYu38xPv0t2kxKM_4DM6QBq0/pubhtml';
+
+    App.table = null;
+    App.rows = null;
+    App.curreltSelection = null;
+
+    App.db = null;
+
+    function setupDB(data, tabletop) {
+
+        /** Add the rows to the database  **/
+        App.rows = tabletop.sheets("Papers").all();
+        App.encodings = tabletop.sheets("Encodings").all();
+        App.tasks = tabletop.sheets("Tasks").all();
+
+        /** Parse the data before inserting it into the database **/
+        _.map(App.rows, function (o) {
+
+            // delete the bibtex entry attribute
+            delete o["Bibtex Entry"];
+
+            // split the fields that are lists
+            o["Data Types"] = o["Data Types"].split(",");
+            _.map(o["Data Types"], _.trimEnd);
+
+            o["Encodings"] = o["Encodings"].split(", ");
+            _.map(o["Encodings"], _.trimEnd);
+
+            o["Tasks"] = o["Tasks"].split(", ");
+            _.map(o["Tasks"], _.trimEnd);
+
+            o["Evaluation Type"] = o["Evaluation Type"].split(", ");
+            _.map(o["Evaluation Type"], _.trimEnd);
+
+            o["Paradigm"] = o["Paradigm"].split(", ");
+            _.map(o["Paradigm"], _.trimEnd);
+
+            o["SubDomain"] = o["SubDomain"].split(", ");
+            _.map(o["SubDomain"], _.trim);
+
+        });
+        /** create the new database for the session **/
+        App.db = DB.initializeDB('BioMed', App.rows);
+    }
+
+    App.initDB = function() {
+
+        /** Access the Google Doc and select grab the  data **/
+        Tabletop.init({
+            key: geospatial_spreadsheet_url,
+            callback: setupDB,
+            wanted: ["Papers", "Tasks", "Encodings"],
+            debug: true
+        });
+    };
+
+    function setupCharts(data){
+
+        /* get the sub-domains from the model */
+        var subDomains = _.map(_.find(App.model.fields(), {property: 'subDomain'} ).elements, 'text' );
+
+        /** initialize a new bubble graph **/
+        /** initialize a new bubble graph **/
+        App.geoGraph = new Graph({
+            colorMap: {
+                "Urban": "#beaed4",
+                "Media Analysis": "#fdc086",
+                "Oceanography": "#7fc97f",
+                "Business": "#7fc97f",
+                "Climate": "#7fc97f",
+                "Surveillance": "#7fc97f",
+                "Environmental Analysis": "#7fc97f",
+                "Cartography": "#7fc97f"
+            },
+            subDomains: subDomains
+        });
+
+        App.dataParser = new Parser({
+            colorMap:
+                [{
+                    "Urban": "#beaed4",
+                    "Media Analysis": "#fdc086",
+                    "Oceanography": "#7fc97f",
+                    "Business": "#7fc97f",
+                    "Climate": "#7fc97f",
+                    "Surveillance": "#7fc97f",
+                    "Environmental Analysis": "#7fc97f",
+                    "Cartography": "#7fc97f"
+                },
+                    {
+                        "Domain Experts" : "#fbb4ae",
+                        "Visualization Experts" : "#b3cde3"
+                    }
+                ],
+                subDomains: subDomains
+        });
+
+
+        // get the parsed encodings
+        var encodingData = App.dataParser.parseEncodings(data);
+        var taskData = App.dataParser.parseFields(data);
+
+        //var testParsing = App.dataParser.parseArbFields(data, "paradigms", "dataTypes");
+
+        // plot the bubble scatter plots
+        App.geoGraph.graphEncodingBubbleNVD3Chart(encodingData.encodings, "#encodings",
+            encodingData.subDomainCount, encodingData.authors,
+            encodingData.xLabelMap, encodingData.yLabelMap);
+
+        //App.geoGraph.graphEncodingBubbleNVD3Chart(testParsing.pairings, "#encodings",
+        //    testParsing.max, testParsing.xDomain, testParsing.authors, testParsing.subDomains);
+
+        // plot the task analysis
+        App.geoGraph.graphTaskBarNVD3Chart(taskData.tasks, "#tasks", taskData.authors[0]);
+
+        // plot the data type analysis
+        App.geoGraph.graphTypeBarNVD3Chart(taskData.dataTypes, "#dataTypes", taskData.authors[1]);
+
+        // // plot the data type analysis
+        App.geoGraph.graphEvaluationNVD3Chart(taskData.evaluation, "#evaluation", taskData.authors[2]);
+
+        // plot the data type analysis
+        App.geoGraph.graphParadigmsNVD3Chart(taskData.paradigms, "#paradigms",  taskData.authors[3]);
+
+        // plot the data type analysis
+        App.geoGraph.graphEvaluatorsNVD3Chart(taskData.evaluators, "#evaluators",
+            ["Table", "Field", "Network", "Geometry"], taskData.authors[4]);
+    }
+
+    function setupTable(data) {
+
+        // TODO Check if there was a previous query and clear the old results
+
+        App.curreltSelection = [];
+        App.queryResults = data;
+
+        // jQuery selector to avoid redundant calls
+        var tableSelector = $('#papers');
+
+        //Reference : https://datatables.net/reference/index
+        $(document).ready(function () {
+
+            /** display the results **/
+            d3.select('#results')
+                .style("display", "block");
+
+            // if the table was already created, refresh it with new data
+            if(App.table)
+            {
+                /* clear the old click events */
+                tableSelector.find('tbody').off("click", 'tr td.details-control');
+
+                /* clear the table and charts */
+                App.table.clear().draw();
+                d3.select("#dataTypes svg").remove();
+                d3.select("#tasks svg").remove();
+                d3.select("#encodings svg").remove();
+                d3.select("#evaluation svg").remove();
+                d3.select("#evaluators svg").remove();
+                d3.select("#paradigms svg").remove();
+
+                /* Add new data */
+                App.table.rows.add(data);
+                /* Draw the DataTable */
+                App.table.columns.adjust().draw();
+            }
+            /* else, create the table */
+            else
+            {
+                App.table = tableSelector.DataTable({
+                    data: data,
+                    scrollY:  '50vh',
+                    scrollX:  false,
+                    sScrollY: null,
+                    "bFilter": false,
+                    columns: [
+                        {
+                            "class":          "details-control",
+                            "orderable":      false,
+                            "data":           null,
+                            "defaultContent": ""
+                        },
+                        {title: "Author", data: "author", className: "dt-center",  "targets": [ 0 ] },
+                        {title: "Year", data: "year", className: "dt-center",  "targets": [ 0 ]},
+                        {title: "Paper Title", data: "title", className: "dt-center",  "targets": [ 0 ]},
+                        // {title: "Url", data: "url"},
+                        // {title: "Domain", data: "domain", className: "dt-center",  "targets": [ 0 ]},
+                        {title: "Sub-Domain", data: "subDomain", className: "dt-center",  "targets": [ 0 ]},
+                        // {title: "No. of Users", data: "# of Users"},
+                        // {title: "Users", data: "Users"},
+                        {title: "Level of Expertise", data: "expertise", className: "dt-center",  "targets": [ 0 ]},
+                        // {title: "Data Types", data: "dataTypes"},
+                        {title: "Paradigm", data: "paradigms", className: "dt-center",  "targets": [ 0 ]},
+                        // {title: "Number of Overlays", data: "Number of Overlays"},
+                        {title: "Evaluation type", data: "evaluation", className: "dt-center",  "targets": [ 0 ]},
+                        {title: "Evaluators", data: "evaluators", className: "dt-center",  "targets": [ 0 ]}
+                    ],
+                    order: [[1, 'asc'], [0, 'asc']]
+                    // stateSave: true
+                });
+            }
+
+            /** formatter for the sub-rows **/
+            function format ( d ) {
+
+                /** construct the sub row details **/
+                var details =
+                    '<div class="details-container">'+
+                    '<table cellpadding="5" cellspacing="0" border="0" class="details-table">'+
+
+                    '<tr>'+
+                    '<td class="title">Paper:</td>'+
+                    '<td><a href="' + d.url +'" target="_blank">' + d.title + '</a></td>'+
+                    '</tr>'+
+
+                    '<tr>'+
+                    '<td class="title">Encodings :</td>'+
+                    '<td>' + d.encodings.join(', ') +'</td>'+
+                    '</tr>'+
+
+                    '<tr>'+
+                    '<td class="title">DataSet Types :</td>'+
+                    '<td>' + d.dataTypes.join(', ') +'</td>'+
+                    '</tr>'+
+
+                    '<tr>'+
+                    '<td class="title">Tasks :</td>'+
+                    '<td>' + d.tasks.join(', ') +'</td>'+
+                    '</tr>'+
+
+                    '</table>'+
+                    '</div>';
+
+                return details;
+            }
+
+            // Array to track the ids of the details displayed rows
+            var detailRows = [];
+
+            /** Setup the sub-row click events **/
+            tableSelector.find('tbody').on( 'click', 'tr td.details-control', function () {
+
+                var tr = $(this).closest('tr');
+                var row = App.table.row( tr );
+                var idx = $.inArray( tr.attr('id'), detailRows );
+
+                if ( row.child.isShown() ) {
+                    tr.removeClass( 'details' );
+                    row.child.hide();
+
+                    // Remove from the 'open' array
+                    detailRows.splice( idx, 1 );
+                }
+                else {
+                    tr.addClass( 'details' );
+                    row.child( format( row.data() ) ).show();
+
+                    // Add to the 'open' array
+                    if ( idx === -1 ) {
+                        detailRows.push( tr.attr('id') );
+                    }
+                }
+            } );
+
+            // On each draw, loop over the `detailRows` array and show any child rows
+            App.table.on( 'draw', function () {
+                $.each( detailRows, function ( i, id ) {
+                    $('#'+id+' td.details-control').trigger( 'click' );
+                } );
+            } );
+
+            /** setup the charts **/
+            if(App.queryResults.length > 0){
+                setupCharts(data);
+
+                d3.selectAll('.resultCharts')
+                    .style("display", "block");
+            }
+            // no results to show
+            else{
+                d3.selectAll('.resultCharts')
+                    .style("display", "none");
+            }
+
+            /** remove the spinner **/
+            d3.select('#loading')
+                .style("display", "none");
+
+            /** move the searchbar to the top **/
+            d3.select("#search")
+                .classed("vcenter", false);
+
+        });
+    }
+
+    App.getResults = function(e) {
+
+        /** put up the loading spinner **/
+        d3.select('#loading')
+            .style("display", "block");
+
+        var input = [], operator = [], values = [],
+            advanced = {
+                domain: [],
+                subDomain: [],
+                dataTypes: [],
+                paradigms: [],
+                encodings: [],
+                evaluators: []
+            };
+
+        /** Check to see if a previous search was performed. If so,
+         *  clear the old charts and table */
+
+        /** Get the search values **/
+
+            // search variable
+        $('[name="my_fields[]"]').each(function(){
+            input.push($(this).val().split()[0]);
+        });
+
+        // operator
+        $('[name="operator[]"]').each(function(){
+            operator.push($(this).val().split()[0]);
+        });
+
+        // value
+        $('[name="my_value[]"]').each(function(){
+            values.push($(this).val().split()[0]);
+        });
+
+        /** Get the advanced values **/
+        $('#accordion').find('input:checked').each(function() {
+            advanced[ $(this).attr('name')].push( $(this).attr('value'));
+        });
+
+        /* minimize the accordion if open */
+        if(d3.select('#accordion li').classed('open')) {
+            App.accordion.dropdown({data: {el: App.accordion.el, multiple: App.accordion.multiple}});
+        }
+
+        /** query the for the papers that meet the criteria **/
+        DB.queryPapers({and: [input, operator, values], or: advanced}, setupTable);
+
+        // we don't want the page to reload
+        return false;
+    };
+
+    /** clears the advanced search menu checkboxes **/
+    App.resetFields = function() {
+
+        $('#accordion').find('input:checked').each(function() {
+            $(this).attr('checked', false);
+        });
+    }
+
+})();
---
+++ geospatial/src/js/model.js
@@ -0,0 +1,139 @@
+var App = App || {};
+
+/*** KO Class ***/
+function Papers() {
+    var self = this;
+
+    /* Advanced search field form data
+     * Each object of the array is a check list */
+    self.fields = ko.observableArray(
+        [
+            {
+                name: 'Domain',
+                elements: [
+                    {text: "Geospatial", category: "domain"},
+                    {text: "Trajectories", category: "domain" }
+                ]
+            },
+            {
+                name: 'SubDomain',
+                property: 'subDomain',
+                elements: [
+                    {text: "Urban", category: "subDomain"},
+                    {text: "Media Analysis", category: "subDomain" },
+                    {text: "Oceanography", category: "subDomain"},
+                    {text: "Business", category: "subDomain"},
+                    {text: "Climate", category: "subDomain"},
+                    {text: "Surveillance", category: "subDomain"},
+                    {text: "Environmental Analysis", category: "subDomain"},
+                    {text: "Cartography", category: "subDomain"},
+                    {text: "Uncertainty", category: "subDomain"},
+                    {text: "Sports", category: "subDomain"},
+                    {text: "Astronomy", category: "subDomain"}
+                ]
+            },
+            {
+                name: 'DataSet Types',
+                property: 'dataTypes',
+                elements: [
+                    {text:"Table", category: "dataTypes"},
+                    {text:"Network", category: "dataTypes"},
+                    {text:"Field", category: "dataTypes"},
+                    {text:"Geometry", category: "dataTypes"},
+                    {text:"Set", category: "dataTypes"},
+                    {text:"Cluster", category: "dataTypes"},
+                    {text:"Path", category: "dataTypes"}
+                ]
+            },
+            {
+                name: 'Tasks',
+                property: 'tasks',
+                elements: [
+                    {text: "Discover", category: 'tasks'},
+                    {text: "Present", category: 'tasks'},
+                    {text: "Annotate", category: 'tasks'},
+                    {text: "Record", category: 'tasks'},
+                    {text: "Derive", category: 'tasks'},
+                    {text: "Browse", category: 'tasks'},
+                    {text: "Locate", category: 'tasks'},
+                    {text: "Explore", category: 'tasks'},
+                    {text: "Lookup", category: 'tasks'},
+                    {text: "Identify", category: 'tasks'},
+                    {text: "Compare", category: 'tasks'},
+                    {text: "Summarize", category: 'tasks'}
+                ]
+            },
+            {
+                name: 'Paradigms',
+                property: 'paradigms',
+                elements: [
+                    {text:"Linked Views", category: "paradigms"},
+                    {text:"Overlays", category: "paradigms"},
+                    {text:"Spatial Nesting" , category: "paradigms"},
+                    {text:"Non-Spatial Nesting", category: "paradigms"}
+                ]
+            },
+            {
+                name: 'Spatial Encodings',
+                property: 'spatial',
+                elements: [
+                    {text:"Simple Map", category: "encodings"},
+                    {text:"Choropheth / Heatmap", category: "encodings"},
+                    {text:"Ball and Stick / Mesh", category: "encodings"},
+                    {text:"Isosurface / Streamlines", category: "encodings"},
+                    {text:"Volume / Images", category: "encodings"},
+                    {text:"Contour", category: "encodings"},
+                    {text:"Glyphs", category: "encodings"},
+                    {text:"Animation", category: "encodings"}
+                ]
+            },
+            {
+                name: 'Non-Spatial Encodings',
+                property: 'nonSpatial',
+                elements: [
+                    {text:"Color", category: "encodings"},
+                    {text:"Label", category: "encodings"},
+                    {text:"Bar Chart", category: "encodings"},
+                    {text:"Line Chart", category: "encodings"},
+                    {text:"Sequence", category: "encodings"},
+                    {text:"Pie Chart", category: "encodings"},
+                    {text:"Histogram", category: "encodings"}
+                ]},
+            {
+                name: '',
+                property: 'nonSpatial',
+                elements: [
+                    {text:"Scatterplot", category: "encodings"},
+                    {text:"Box Plot", category: "encodings"},
+                    {text:"Node-Link", category: "encodings"},
+                    {text:"Scatterplot Matrix", category: "encodings"},
+                    {text:"Parallel Coordinates", category: "encodings"},
+                    {text:"Matrix", category: "encodings"}
+                ]
+            },
+            {
+                name: 'Evaluators',
+                elements: [
+                    {text:"Domain Experts", category: "evaluators"},
+                    {text:"Visualization Experts", category: "evaluators"}
+                ]
+            }
+        ]
+    );
+
+    self.attributes = ko.observableArray(
+        [
+            "Tasks",
+            "Paradigms",
+            "DataTypes",
+            "Spatial Encodings",
+            "Non-Spatial Encodings"
+        ]
+    );
+}
+
+/*** IFE to load the data and apply the KO bindings ***/
+(function(){
+    App.model = new Papers();
+    ko.applyBindings(new Papers());
+})();
